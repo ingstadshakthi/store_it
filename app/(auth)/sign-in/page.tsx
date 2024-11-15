77e1a8d7418967192dfa95e03e31a1492a7ac637
ingstadshakthi <59392423+ingstadshakthi@users.noreply.github.com>
2024-11-16 00:01:28 +0530

    reusable auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,5 @@
+import AuthForm from '@/components/AuthForm';
+
 export default function SignInPage() {
-  return <div>Sign In</div>;
+  return <AuthForm type='sign-in' />;
 }
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,3 +1,5 @@
+import AuthForm from '@/components/AuthForm';
+
 export default function SignUpPage() {
-  return <div>Sign Up</div>;
+  return <AuthForm type='sign-up' />;
 }
